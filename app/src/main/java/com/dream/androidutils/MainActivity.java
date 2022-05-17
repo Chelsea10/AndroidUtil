013030fd073779ddcdef78a0bab26840dd8b59f5
chenmingxin <chenmingxin@mindlinder.com>
2022-05-17 17:54:43 +0800

    [modify][what]第一次代码提交

---
+++ app/src/main/java/com/dream/androidutils/MainActivity.java
@@ -0,0 +1,18 @@
+package com.dream.androidutils;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        ToastUtils.showShortToast(this,"123456789");
+
+    }
+}
